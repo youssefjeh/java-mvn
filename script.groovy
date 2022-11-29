cd113b1f1deccbb089a849d7307a18c2bf549895
Youssef Jehbali <85554678+youssefjeh@users.noreply.github.com>
2022-11-29 18:13:54 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,24 @@
-def buildApp() {
-     echo 'building app...'
+def buildJar() {
+     echo 'building Jar file...'
+     sh 'mvn package'
 }
 
-def testApp() {
-     echo 'testing app...'
+def buildJar() {
+     echo 'building docker image..'
+     withCredential([usernamePassword(credentialsId:'dockerhub-rep', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t dockerysf/my-app:3.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push dockerysf/my-app:3.0'
+     }
 }
+
+/*def testApp() {
+     echo 'testing app...'
+}*/
 
 def deployApp() {
      echo 'deploying app...'
-     echo "deploying version ${params.VERSION}"
+     // echo "deploying version ${params.VERSION}"
 }
 
 return this
